34381a94583fcd1373c45b321a21fd0bc90b562d
andrii-tymoshchenko <andrey.timoschenko@gmail.com>
2023-05-20 00:40:09 +0300

    Added ability to specify GPIO device and line num
    Signed-off-by: andrii-tymoshchenko <andrey.timoschenko@gmail.com>

--- test-code-c/testGPIO/src/main.c
+++ test-code-c/testGPIO/src/main.c
@@ -5,36 +5,54 @@
 #include <errno.h>      // Error integer and strerror() function
 #include "gpiod.h"
 
+char *used_chipname = "/dev/gpiochip0";     //default device address
+unsigned int gpio_num = 10;                 //default gpio line
+struct gpiod_chip *chip;
+struct gpiod_line *line;
 extern int errno;
 
-int main()
+int main(int argc, char *argv[])
 {
+    int opt;
+    while ((opt = getopt(argc, argv, "d:l:")) != EOF){
+        switch(opt){
+            case 'd':
+                printf("Selected device address: \"%s\"\n", optarg);
+                used_chipname = optarg;
+                break;
+            case 'l':
+                printf("Selected gpio line: \"%s\"\n", optarg);
+                gpio_num = atoi(optarg);
+                break;
+            default:
+                printf("Usage:\n "
+                       "1: no parameters, will be used default /dev/gpiochip0 device address and default gpio line 10 (PA10)\n"
+                       "2: 'programm_name -d /dev/gpiochipN -l L', where N - gpio chip number, L - gpio line number\n"
+                       "3: selected only -d option with argument\n"
+                       "4: selected only -l option with argument\n");
+                break;
+        }
+    }
+
     printf("+---------------------------------+\n");
     printf("| Test Embedded Linux GPIO (LEDs) |\n");
-    printf("|   - blink GPIO10 (PA10)         |\n");
     printf("+---------------------------------+\n");
 
-    const char *chipname = "/dev/gpiochip0";     //device address
-    const unsigned int gpio_num = 10;
-    struct gpiod_chip *chip;
-    struct gpiod_line *line;
-    int req;
-
-    chip = gpiod_chip_open(chipname);
+    chip = gpiod_chip_open(used_chipname);
     if (!chip){
         printf("Getting chip error: [%i] %s\n", errno, strerror(errno));
-        return -1;
+        return EXIT_FAILURE;
     }
-    printf("%s Opened Successfully\n", chipname);
+    printf("%s Opened Successfully\n", used_chipname);
 
-    line = gpiod_chip_get_line(chip, gpio_num);   //GPIO10(PA10)
+    line = gpiod_chip_get_line(chip, gpio_num);   //GPIO10(PA18)
     if (!line) {
         printf("Getting line error: [%i] %s\n", errno, strerror(errno));
         gpiod_chip_close(chip);
-        return -1;
+        return EXIT_FAILURE;
     }
 
-    req = gpiod_line_request_output(line, "gpio_test", 0);
+    int req = gpiod_line_request_output(line, "gpio_test", 0);
     if(req < 0){
         printf("Requesting gpio line to output error: [%i] %s\n", errno, strerror(errno));
         gpiod_line_release(line);
